efed69e0fe89ae8eccf2a806cdb0de375dbf3d9e
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-07-24 14:21:19 +1000

    Update resid_fnl.py

--- Artemis/plotting/resid_fnl.py
+++ Artemis/plotting/resid_fnl.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(threshold=np.nan)
 from signal_funcs import *
-#Firstly import all modules needed. Should I make resid_fnl.py a function? I think that would be best.
+#Firstly import all modules needed. Should I make resid_fnl.py a function? I think that would be best. done
+#Then calculate halo resids using a function from signal_funcs.py DONE
 #then import all files needed which are: grid_mthd.py output as well as dotprod.py output.
-#Then calculate halo resids
 #Then plot signals as I do in per_snapshot plots (from plot_fig_5.py)
 #then plot 
 
@@ -14,13 +14,16 @@ a_x=np.sort(a_x)
 a_y=np.random.rand(len(a_x))+np.random.randint(5)
 a_err_pos=np.random.rand(len(a_x))+a_y
 a_err_neg=a_y-np.random.rand(len(a_x))
+#a_no_halos=np.random.randint(10000,size=len(a_x))
+a_no_halos=np.zeros(len(a_x))+5000
 
 b_x=np.random.rand(20)*10-10
 b_x=np.sort(b_x)
 b_y=np.random.rand(len(b_x)) 
 b_err_pos=np.random.rand(len(b_x))+b_y
 b_err_neg=b_y-np.random.rand(len(b_x))
-
+#b_no_halos=np.random.randint(10000,size=len(b_x))
+b_no_halos=np.zeros(len(b_x))+6000
 '''
 #non-random data
 a_x=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
@@ -63,11 +66,23 @@ a_err_neg=wdm_err_mns
 b_err_pos=lcdm_err_pos
 b_err_neg=lcdm_err_mns
 '''    
-res,err_pos,err_neg=calc_resid(a_x,a_y,a_err_pos,a_err_neg,b_x,b_y,b_err_pos,b_err_neg)    
+
+#halo count resid
+sig_type='signal'
+a_b_res,a_b_x=subtract_grad(a_x,a_no_halos,b_x,b_no_halos,sig_type='signal')
+b_a_res,b_a_x=subtract_grad(b_x,b_no_halos,a_x,a_no_halos,sig_type='signal')
+res_tot_y=np.hstack((a_b_res,b_a_res))
+res_tot_x=np.hstack((a_b_x,b_a_x))
+res_nohalos=np.column_stack((res_tot_x,res_tot_y))#x,y columns
+res_nohalos = res_nohalos[res_nohalos[:,0].argsort()]
+#signal resid
+res,err_pos,err_neg=calc_resid(a_x,a_y,a_err_pos,a_err_neg,b_x,b_y,b_err_pos,b_err_neg)  
+  
 #plotting
-plt.figure(figsize=(10,7))
+plt_buffr=0.5
+plt.figure(figsize=(10,10))
 #original signals  
-ax1=plt.subplot2grid((2,1), (0,0))
+ax1=plt.subplot2grid((4,1), (0,0))
 ax1.axhline(y=0, xmin=0, xmax=15, color = 'k',linestyle='--')
 ax1.plot(a_x,a_y,'r-',label='a')
 ax1.fill_between(a_x, a_err_neg, a_err_pos,facecolor='red',alpha=0.3)
@@ -75,14 +90,31 @@ ax1.plot(b_x,b_y,'b-',label='b')
 ax1.fill_between(b_x, b_err_neg, b_err_pos,facecolor='blue',alpha=0.3)
 plt.title('signals')
 ax1.legend(loc='best')
-#plt.xlim(np.min(res_lcdm_wdm_np[:,0])-1,np.max(res_lcdm_wdm_np[:,0])+1)#an important question is how to 
+plt.xlim(np.min(np.hstack((a_x,b_x)))-plt_buffr,np.max(np.hstack((a_x,b_x)))+plt_buffr)
 plt.grid()
 #residual
-ax2=plt.subplot2grid((2,1), (1,0))
+ax2=plt.subplot2grid((4,1), (1,0))
 ax2.axhline(y=0, xmin=0, xmax=15, color = 'k',linestyle='--')
 ax2.plot(res[:,0],res[:,1],'g-',label='wdm resid')
 ax2.fill_between(res[:,0], err_neg, err_pos,facecolor='green',alpha=0.3)
 plt.title('residuals')
-#plt.xlim(np.min(res_lcdm_wdm_np[:,0])-1,np.max(res_lcdm_wdm_np[:,0])+1)
+plt.xlim(np.min(np.hstack((a_x,b_x)))-plt_buffr,np.max(np.hstack((a_x,b_x)))+plt_buffr)
+plt.grid()
+#plot no-halos
+ax3=plt.subplot2grid((4,1), (2,0))
+ax3.axhline(y=0, xmin=0, xmax=15, color = 'k',linestyle='--')
+ax3.plot(a_x,a_no_halos,'r-',label='a')
+ax3.plot(b_x,b_no_halos,'b-',label='b')
+plt.title('number of halos')
+plt.xlim(np.min(np.hstack((a_x,b_x)))-plt_buffr,np.max(np.hstack((a_x,b_x)))+plt_buffr)
+#plt.ylim(0,10000)
 plt.grid()
 
+#plot no-halos residuals
+ax4=plt.subplot2grid((4,1), (3,0))
+ax4.axhline(y=0, xmin=0, xmax=15, color = 'k',linestyle='--')
+ax4.plot(res_nohalos[:,0],res_nohalos[:,1],'g-',label='resid')
+plt.title('residuals')
+plt.xlim(np.min(np.hstack((a_x,b_x)))-plt_buffr,np.max(np.hstack((a_x,b_x)))+plt_buffr)
+#plt.ylim(0,10000)
+plt.grid()
