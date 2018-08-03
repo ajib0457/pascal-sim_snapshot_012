3dacb481af2b290554a0e1316f704b8fcedbf236
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-08-03 16:54:21 +1000

    Update dotprod.py

--- Artemis/correl/main_codes/dotprod.py
+++ Artemis/correl/main_codes/dotprod.py
@@ -14,13 +14,15 @@ from decimal import Decimal
 
 sim_type=sys.argv[1]          #'dm_only' 'DTFE'
 cosmology=sys.argv[2]          #DMONLY:'lcdm'  'cde0'  'wdm2'DMGAS: 'lcdm' 'cde000' 'cde050' 'cde099'
-snapshot=int(sys.argv[3])      #'12  '11'...
+snapshot=sys.argv[3]      #'12  '11'...
 den_type=sys.argv[4]           #'DTFE' 'my_den'
 smooth_scl=Decimal(sys.argv[5])#Smoothing scale in physical units Mpc/h. 2  3.5  5
-tot_mass_bins=int(sys.argv[6])      #Number of Halo mass bins
+tot_mass_bins=int(sys.argv[6])      #Number of Halo mass bins MIN=3 for 'increm'
 particles_filt=int(sys.argv[7])   #Halos to filter out based on number of particles, ONLY for Dot Product Spin-LSS(SECTION 5.)
 total_lss_parts=int(sys.argv[8])    #Total amount of lss_class parts
-
+dp_mthd=sys.argv[9]     # 'increm', 'hiho' & 'subdiv'
+bin_overlap=int(sys.argv[10]) #'increm' bins overlap in % (percentage) 
+   
 sim_sz=500           #Size of simulation in physical units Mpc/h cubed
 grid_nodes=1250      #Density Field grid resolution
 lss_type=2           #Cluster-3 Filament-2 Sheet-1 Void-0
@@ -28,6 +30,7 @@ runs=100000          #bootstrap resampling runs
 method='bootstrap'   #Not optional for this code
 hist_bins=200        #histogram to form gaussian pdf
 sigma_area=0.3413    #sigma percentile area as decimal
+
 
 recon_vecs_x=np.zeros((grid_nodes**3))
 recon_vecs_y=np.zeros((grid_nodes**3))
@@ -97,46 +100,181 @@ del mask
 #(Vir. Rad)Rvir(kpc/h), mask(0,1,2,3) & eigvecs(ex,ey,ez) 
 fil_filt=np.where(data[:,12]==lss_type)#2-filament
 data=data[fil_filt]
-  
-halo_mass=data[:,9]
-log_halo_mass=np.log10(halo_mass)#convert into log10(M)
-mass_intvl=(np.max(log_halo_mass)-np.min(log_halo_mass))/tot_mass_bins#log_mass value used to find mass interval
-diction={}
-diction_2={}#This dictionary contains all of the means of all of the samples taken from the dataset of dp's.
-results=np.zeros((tot_mass_bins,5))# [Mass_min, Mass_max, Value, Error+,Error-] 
-for mass_bin in range(tot_mass_bins):
-    
-    low_int_mass=np.min(log_halo_mass)+mass_intvl*mass_bin#Calculate mass interval
-    hi_int_mass=low_int_mass+mass_intvl#Calculate mass interval
-    results[mass_bin,0]=low_int_mass#Store mass interval
-    results[mass_bin,1]=hi_int_mass#Store mass interval   
-    #Create mask to filter out halos within mass interval
-    mass_mask=np.logical_and(log_halo_mass>=low_int_mass,log_halo_mass<hi_int_mass)
-    data_mass_bin=data[mass_mask]
 
-    #normalize vectors
-    data_mass_bin[:,13:16]=skl.normalize(data_mass_bin[:,13:16])#Eigenvectors
-    data_mass_bin[:,6:9]=skl.normalize(data_mass_bin[:,6:9])#Halo AM
-    store_dp=np.zeros(len(data_mass_bin))
-    for i in range(len(data_mass_bin)):
-        store_dp[i]=np.inner(data_mass_bin[i,13:16],data_mass_bin[i,6:9])#take the dot product between vecs, row by row   
-    store_dp=abs(store_dp)    
-    diction[mass_bin]=store_dp    
+#At this point is where the methods will be different.=================================================================================
+
+
+def dpbins_hiho(data):
     
-    #Calculating error using bootstrap resampling
-    a=np.random.randint(low=0,high=len(store_dp),size=(runs,len(store_dp)))
-    mean_set=np.mean(store_dp[a],axis=1)
+    tot_vals=len(data)    
+   
+       #Bin tuning for hiho
+    len_move=150 #bin movement in terms of qty halos in-out. Keep as input
+    no_subset=200 #Ideal amount of qty per bin. 
+    bin_cutoff=5#input bin qty cutoff
+    no_bins=int(1.0*(tot_vals-no_subset)/len_move+1)#to calculate movement steps
     
-    a=np.histogram(mean_set,density=True,bins=hist_bins)
-    x=a[1]
-    dx=(np.max(x)-np.min(x))/hist_bins
-    x=np.delete(x,len(x)-1,0)+dx/2
-    y=a[0]
-    perc_lo,results[mass_bin,4],perc_hi,results[mass_bin,3],results[mass_bin,2],area=error(x,y,dx,sigma_area)
+    if tot_vals<10:
+        no_bins=0 #in order to halt the code
+#        print 'not enough halos'
+    else:
+        while no_bins<bin_cutoff:
+            no_subset-=1
+            no_bins=int(1.0*(tot_vals-no_subset)/len_move+1)#to calculate movement steps
+   
+    data = data[data[:,9].argsort()]#Sort catalog by mass    
     
-    diction_2[mass_bin]=mean_set
-         
-    diction['results']=results
+    diction={}
+    diction_2={}#This dictionary contains all of the means of all of the samples taken from the dataset of dp's.
+    results=np.zeros((no_bins,4))# [bin_centre(mean), Value, Error+,Error-] 
+    for mass_bin in range(no_bins):
+        
+        data_mass_bin=data[len_move*mass_bin:no_subset+len_move*mass_bin]
+        results[mass_bin,0]=np.mean(np.log10(data_mass_bin[:,9]))
+        #normalize vectors
+        data_mass_bin[:,13:16]=skl.normalize(data_mass_bin[:,13:16])#Eigenvectors
+        data_mass_bin[:,6:9]=skl.normalize(data_mass_bin[:,6:9])#Halo AM
+        store_dp=np.zeros(len(data_mass_bin))
+        for i in range(len(data_mass_bin)):
+            store_dp[i]=np.inner(data_mass_bin[i,13:16],data_mass_bin[i,6:9])#take the dot product between vecs, row by row   
+        store_dp=abs(store_dp)    
+        diction[mass_bin]=store_dp    
+        
+        #Calculating error using bootstrap resampling
+        a=np.random.randint(low=0,high=len(store_dp),size=(runs,len(store_dp)))
+        mean_set=np.mean(store_dp[a],axis=1)
+        
+        a=np.histogram(mean_set,density=True,bins=hist_bins)
+        x=a[1]
+        dx=(np.max(x)-np.min(x))/hist_bins
+        x=np.delete(x,len(x)-1,0)+dx/2
+        y=a[0]
+        perc_lo,results[mass_bin,3],perc_hi,results[mass_bin,2],results[mass_bin,1],area=error(x,y,dx,sigma_area)
+        
+        diction_2[mass_bin]=mean_set
+             
+        diction['results']=results
+
+    return diction,diction_2
+
+def dpbins_increm():     
+
+    halo_mass=data[:,9]
+    log_halo_mass=np.log10(halo_mass)#convert into log10(M)        
+    
+    mass_max=np.max(log_halo_mass)
+    mass_min=np.min(log_halo_mass)
+    
+    bin_wdth=(mass_max-mass_min)/(2*(1-(bin_overlap/100.0))+(tot_mass_bins-2)*(1-(2*bin_overlap/100.0))+(tot_mass_bins-1)*(bin_overlap/100.0))
+    len_move=(1-bin_overlap/100.0)*bin_wdth    
+    
+    diction={}
+    diction_2={}#This dictionary contains all of the means of all of the samples taken from the dataset of dp's.
+    results=np.zeros((tot_mass_bins,6))# [Mass_min, Mass_max, Value, Error+,Error-,bin_centre] 
+    for mass_bin in range(tot_mass_bins):
+        low_int_mass=mass_min+mass_bin*len_move
+        hi_int_mass=low_int_mass+bin_wdth+0.000000001
+    
+        results[mass_bin,0]=low_int_mass#Store mass interval
+        results[mass_bin,1]=hi_int_mass#Store mass interval  
+        results[mass_bin,5]=low_int_mass+(hi_int_mass-low_int_mass)/2 #bin value, at the end to avoid changing other 
+        
+        #Create mask to filter out halos within mass interval
+        mass_mask=np.logical_and(log_halo_mass>=low_int_mass,log_halo_mass<hi_int_mass)
+        data_mass_bin=data[mass_mask]
+    
+        #normalize vectors
+        data_mass_bin[:,13:16]=skl.normalize(data_mass_bin[:,13:16])#Eigenvectors
+        data_mass_bin[:,6:9]=skl.normalize(data_mass_bin[:,6:9])#Halo AM
+        store_dp=np.zeros(len(data_mass_bin))
+        for i in range(len(data_mass_bin)):
+            store_dp[i]=np.inner(data_mass_bin[i,13:16],data_mass_bin[i,6:9])#take the dot product between vecs, row by row   
+        store_dp=abs(store_dp)    
+        diction[mass_bin]=store_dp    
+        
+        #Calculating error using bootstrap resampling
+        a=np.random.randint(low=0,high=len(store_dp),size=(runs,len(store_dp)))
+        mean_set=np.mean(store_dp[a],axis=1)
+        
+        a=np.histogram(mean_set,density=True,bins=hist_bins)
+        x=a[1]
+        dx=(np.max(x)-np.min(x))/hist_bins
+        x=np.delete(x,len(x)-1,0)+dx/2
+        y=a[0]
+        perc_lo,results[mass_bin,4],perc_hi,results[mass_bin,3],results[mass_bin,2],area=error(x,y,dx,sigma_area)
+        
+        diction_2[mass_bin]=mean_set
+             
+        diction['results']=results
+        
+    return diction,diction_2
+ 
+def dpbins_subdiv():
+    
+    halo_mass=data[:,9]
+    log_halo_mass=np.log10(halo_mass)#convert into log10(M)
+    
+    mass_intvl=(np.max(log_halo_mass)-np.min(log_halo_mass))/tot_mass_bins#log_mass value used to find mass interval
+    diction={}
+    diction_2={}#This dictionary contains all of the means of all of the samples taken from the dataset of dp's.
+    results=np.zeros((tot_mass_bins,5))# [Mass_min, Mass_max, Value, Error+,Error-] 
+    for mass_bin in range(tot_mass_bins):
+        
+        low_int_mass=np.min(log_halo_mass)+mass_intvl*mass_bin#Calculate mass interval
+        hi_int_mass=low_int_mass+mass_intvl+0.000000001#Calculate mass interval
+        results[mass_bin,0]=low_int_mass#Store mass interval
+        results[mass_bin,1]=hi_int_mass#Store mass interval   
+        #Create mask to filter out halos within mass interval
+        mass_mask=np.logical_and(log_halo_mass>=low_int_mass,log_halo_mass<hi_int_mass)
+        data_mass_bin=data[mass_mask]
+    
+        #normalize vectors
+        data_mass_bin[:,13:16]=skl.normalize(data_mass_bin[:,13:16])#Eigenvectors
+        data_mass_bin[:,6:9]=skl.normalize(data_mass_bin[:,6:9])#Halo AM
+        store_dp=np.zeros(len(data_mass_bin))
+        for i in range(len(data_mass_bin)):
+            store_dp[i]=np.inner(data_mass_bin[i,13:16],data_mass_bin[i,6:9])#take the dot product between vecs, row by row   
+        store_dp=abs(store_dp)    
+        diction[mass_bin]=store_dp    
+        
+        #Calculating error using bootstrap resampling
+        a=np.random.randint(low=0,high=len(store_dp),size=(runs,len(store_dp)))
+        mean_set=np.mean(store_dp[a],axis=1)
+        
+        a=np.histogram(mean_set,density=True,bins=hist_bins)
+        x=a[1]
+        dx=(np.max(x)-np.min(x))/hist_bins
+        x=np.delete(x,len(x)-1,0)+dx/2
+        y=a[0]
+        perc_lo,results[mass_bin,4],perc_hi,results[mass_bin,3],results[mass_bin,2],area=error(x,y,dx,sigma_area)
+        
+        diction_2[mass_bin]=mean_set
+             
+        diction['results']=results
+        
+    return diction,diction_2
+            
+if dp_mthd=='subdiv':
+    diction,diction_2=dpbins_subdiv()
+    filehandler = open('/scratch/GAMNSCM2/%s/%s/snapshot_0%s/correl/%s/files/dotproduct/spin_lss/%s_snapshot_0%s_dp_LSS%s_spin_sim%sMpc_grid%s_smth%sMpc_%sbins_partclfilt%s_dpmthd_%s.pkl'%(sim_type,cosmology,snapshot,den_type,cosmology,snapshot,lss_type,sim_sz,grid_nodes,smooth_scl,tot_mass_bins,particles_filt,dp_mthd),"wb")
+
+if dp_mthd=='increm':
+    diction,diction_2=dpbins_increm()
+    filehandler = open('/scratch/GAMNSCM2/%s/%s/snapshot_0%s/correl/%s/files/dotproduct/spin_lss/%s_snapshot_0%s_dp_LSS%s_spin_sim%sMpc_grid%s_smth%sMpc_%sbins_partclfilt%s_dpmthd_%s_overlap%sperc.pkl'%(sim_type,cosmology,snapshot,den_type,cosmology,snapshot,lss_type,sim_sz,grid_nodes,smooth_scl,tot_mass_bins,particles_filt,dp_mthd,bin_overlap),"wb")
+
+if dp_mthd=='hiho':
+    diction,diction_2=dpbins_hiho(data)
+    filehandler = open('/scratch/GAMNSCM2/%s/%s/snapshot_0%s/correl/%s/files/dotproduct/spin_lss/%s_snapshot_0%s_dp_LSS%s_spin_sim%sMpc_grid%s_smth%sMpc_%sbins_partclfilt%s_dpmthd_%s.pkl'%(sim_type,cosmology,snapshot,den_type,cosmology,snapshot,lss_type,sim_sz,grid_nodes,smooth_scl,tot_mass_bins,particles_filt,dp_mthd),"wb")
+
+   
+pickle.dump(diction,filehandler)
+filehandler.close() 
+   
+#Plot correlation    
+alignment_plt(grid_nodes,diction['results'],smooth_scl,sim_type,cosmology,snapshot,den_type,dp_mthd,bin_overlap=bin_overlap)
+  
+posterior_plt(cosmology,diction_2,diction['results'],hist_bins,sim_sz,grid_nodes,smooth_scl,tot_mass_bins,particles_filt,lss_type,method,sim_type,snapshot,den_type,dp_mthd,bin_overlap=bin_overlap)
+ 
 '''       
 f=h5py.File('/scratch/GAMNSCM2/bolchoi_z0/correl/my_den/files/output_files/dotproduct/spin_lss/myden_dp_LSS%s_spin_sim%sMpc_grid%s_smth%sMpc_%sbins_partclfilt%s.h5'%(lss_type,sim_sz,grid_nodes,smooth_scl,tot_mass_bins,particles_filt),'w')     
 for mass_bins in range(tot_mass_bins):   
@@ -144,11 +282,3 @@ for mass_bins in range(tot_mass_bins):
 f.create_dataset('/results',data=results)
 f.close()
 '''
-
-filehandler = open('/scratch/GAMNSCM2/%s/%s/snapshot_0%s/correl/%s/files/dotproduct/spin_lss/%s_snapshot_0%s_dp_LSS%s_spin_sim%sMpc_grid%s_smth%sMpc_%sbins_partclfilt%s.pkl'%(sim_type,cosmology,snapshot,den_type,cosmology,snapshot,lss_type,sim_sz,grid_nodes,smooth_scl,tot_mass_bins,particles_filt),"wb")
-pickle.dump(diction,filehandler)    
-#Plot correlation    
-alignment_plt(grid_nodes,results,smooth_scl,sim_type,cosmology,snapshot,den_type)
-  
-posterior_plt(cosmology,diction_2,results,hist_bins,sim_sz,grid_nodes,smooth_scl,tot_mass_bins,particles_filt,lss_type,method,sim_type,snapshot,den_type)
- 
