93c4ef469d23531371a0e039d87dea4998be4eb0
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-09-28 17:57:32 +1000

    Update halo_halo_alignment.py

--- Artemis/correl/main_codes/halo_halo_alignment.py
+++ Artemis/correl/main_codes/halo_halo_alignment.py
@@ -1,9 +1,9 @@
 import numpy as np
-import math as mth
-import pandas as pd
-import sklearn.preprocessing as skl
+import matplotlib
 import h5py
 from scipy.spatial import distance
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 sim_type='dm_only'   #'dm_only' 'DTFE'
 cosmology='lcdm'     #DMONLY:'lcdm'  'cde0'  'wdm2'DMGAS: 'lcdm' 'cde000' 'cde050' 'cde099'
@@ -13,20 +13,19 @@ smooth_scl=2       #Smoothing scale in physical units Mpc/h. 2  3.5  5
 sim_sz=500           #Size of simulation in physical units Mpc/h cubed
 grid_nodes=1250      #Density Field grid resolution
 
-f=h5py.File("%s_%s_snapshot_0%s_pascal_VELOCIraptor_allhalos_xyz_vxyz_jxyz_mtot_r_npart.h5"%(sim_type,cosmology,snapshot), 'r')
+f=h5py.File("/scratch/GAMNSCM2/%s/%s/snapshot_0%s/catalogs/%s_%s_snapshot_0%s_pascal_VELOCIraptor_allhalos_xyz_vxyz_jxyz_mtot_r_npart.h5"%(sim_type,cosmology,snapshot,sim_type,cosmology,snapshot), 'r')
 data=f['/halo'][:]#halos array: (Pos)XYZ(Mpc/h), (Vel)VxVyVz(km/s), (Ang. Mom)JxJyJz((Msun/h)*(kpc/h)*km/s), (tot. Mass)Mtot(10^10Msun/h),(Vir. Rad)Rvir(kpc/h) & npart (no. particles for each sructure)
 f.close()
-no_halos=len(data)-224000
-#dist=np.zeros(((no_halos-1)**2+no_halos-1)/2)
-#no_dist=no_halos*(no_halos+1)/2.0
-#print "{:,}".format(no_dist)
-dist_sp=distance.pdist(data[0:50000,0:3], 'euclidean')
+#no_halos=len(data)
 
-'''
-k=0
-for i in range(no_halos-1):
-     for j in range(i+1,no_halos):
-         
-         dist[k]=np.sqrt((data[i,0]-data[j,0])**2+(data[i,1]-data[j,1])**2+(data[i,2]-data[j,2])**2)
-         k+=1
-'''
+dist_sp=distance.pdist(data[:,0:3], 'euclidean')
+bn=np.logical_and(dist_sp>=0.06,dist_sp<0.1)
+a=np.where(bn==True)
+
+x,y=np.shape(a)
+min_dis=np.min(dist_sp)
+plt.hist(dist_sp,bins=200,label='%s__min%s'%(y,round(min_dis,3)))
+plt.xlabel('halo distance (Mpc/h)')
+plt.ylabel('qty')
+plt.legend()
+plt.savefig('halo_halo_alignment.png')
