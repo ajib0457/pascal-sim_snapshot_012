b46456d05749caabcfab2838296cd23c8ae2caaa
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-10-02 15:51:28 +1000

    Update halo_halo_alignment.py

--- Artemis/correl/main_codes/halo_halo_alignment.py
+++ Artemis/correl/main_codes/halo_halo_alignment.py
@@ -1,8 +1,6 @@
 import numpy as np
-import matplotlib
 import h5py
 from scipy.spatial import distance
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 sim_type='dm_only'   #'dm_only' 'DTFE'
@@ -13,15 +11,42 @@ smooth_scl=2       #Smoothing scale in physical units Mpc/h. 2  3.5  5
 sim_sz=500           #Size of simulation in physical units Mpc/h cubed
 grid_nodes=1250      #Density Field grid resolution
 
-f=h5py.File("/scratch/GAMNSCM2/%s/%s/snapshot_0%s/catalogs/%s_%s_snapshot_0%s_pascal_VELOCIraptor_allhalos_xyz_vxyz_jxyz_mtot_r_npart.h5"%(sim_type,cosmology,snapshot,sim_type,cosmology,snapshot), 'r')
+f=h5py.File("%s_%s_snapshot_0%s_pascal_VELOCIraptor_allhalos_xyz_vxyz_jxyz_mtot_r_npart.h5"%(sim_type,cosmology,snapshot), 'r')
 data=f['/halo'][:]#halos array: (Pos)XYZ(Mpc/h), (Vel)VxVyVz(km/s), (Ang. Mom)JxJyJz((Msun/h)*(kpc/h)*km/s), (tot. Mass)Mtot(10^10Msun/h),(Vir. Rad)Rvir(kpc/h) & npart (no. particles for each sructure)
 f.close()
 #no_halos=len(data)
+a_len=5000
+dist_sp=distance.pdist(data[0:a_len,0:3], 'euclidean')
 
-dist_sp=distance.pdist(data[:,0:3], 'euclidean')
-bn=np.logical_and(dist_sp>=0.06,dist_sp<0.1)
+bn=np.logical_and(dist_sp>=8,dist_sp<15)
+
 a=np.where(bn==True)
+a=np.asarray(a)
+dist_sp=dist_sp[a]
+l,w=np.shape(a)
+indxs=np.zeros((w))
+indxd=np.zeros((w))
 
+for j in range(w): 
+    i=0
+    div=a[0,j]-(a_len-1)    
+    while div>=0:
+        i+=1
+        div=div-(a_len-1-i)
+    indxs[j]=i    
+    indxd[j]=indxs[j]+1+div+(a_len-1-i)
+
+
+#test distances
+dist_sp_test=np.zeros((len(indxs)))
+for i in range(len(indxs)):
+    
+    dist_sp_test[i]=np.sqrt((data[int(indxs[i]),0]-data[int(indxd[i]),0])**2+(data[int(indxs[i]),1]-data[int(indxd[i]),1])**2+(data[int(indxs[i]),2]-data[int(indxd[i]),2])**2)
+
+print w
+print np.sum(dist_sp_test-dist_sp)
+        
+'''
 x,y=np.shape(a)
 min_dis=np.min(dist_sp)
 plt.hist(dist_sp,bins=200,label='%s__min%s'%(y,round(min_dis,3)))
@@ -29,3 +54,6 @@ plt.xlabel('halo distance (Mpc/h)')
 plt.ylabel('qty')
 plt.legend()
 plt.savefig('halo_halo_alignment.png')
+'''
+
+
