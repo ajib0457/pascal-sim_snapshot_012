68802f3d2614d74f0768e246dfadeff0161a4a7d
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-10-15 16:05:47 +1100

    Update halo_halo_dpmr.py

--- Artemis/correl/main_codes/halo_halo/halo_halo_dpmr.py
+++ Artemis/correl/main_codes/halo_halo/halo_halo_dpmr.py
@@ -2,7 +2,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
-snap='10'
+snap='09'
 cosm='lcdm'
 filehandler = open('/import/oth3/ajib0457/pascal_sims/halo_halo_plts/dm_only/%s/resultsdp_dm_only_%s_%s_100.pkl'%(cosm,cosm,snap),"rb")       
 results_dp=pickle.load(filehandler)
@@ -23,7 +23,7 @@ ax.set_ylabel('<J(x).J(x+r)>')
 plt.legend()
 '''
 
-
+'''
 plt.figure()#plot original dp vs dist
 ax2=plt.subplot2grid((1,1), (0,0))
 #ax2.axhline(y=0.5, xmin=0, xmax=15, color = 'k',linestyle='--')
@@ -36,7 +36,7 @@ ax2.fill_between(results_mr[:,0], results_mr[:,2]-abs(results_mr[:,4]), results_
 #ax2.fill_between(results_dp[:,0], results_dp[:,2]-abs(results_dp[:,4]), results_dp[:,2]+abs(results_dp[:,3]),facecolor='red',alpha=0.3)
 
 plt.legend(loc='upper right')
-
+'''
 
 '''
 plt.figure()#plot mr vs dp
